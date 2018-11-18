7a0822b359adb06bfaceba729808f21c20d9ce4f
Marc Mills <mdmills1990@gmail.com>
2018-11-18 18:53:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Marc Mills."
+  end
+
+  get '/hometown' do
+    "My hometown is Freehold, New Jersey."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is the Super Mario bros theme. Lol."
+  end
 end
